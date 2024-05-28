2825f464a41b8d93fb6c3b34031c13c7ffc82f4e
Michal Trojanowski <mtroja98@gmail.com>
2024-05-28 13:48:57 +0200

    test

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        String[] input = takeUserInput();
+    }
+
+    private static String[] takeUserInput() {
+        while(true){
+            System.out.println("Please enter random words separated by space: ");
+
+            Scanner sc = new Scanner(System.in);
+            String input = sc.next();
+
+        }
+    }
+}
