f455be3ce519e901c2d07ffb50c44cd770d54f55
Michal Trojanowski <mtroja98@gmail.com>
2024-05-28 14:11:23 +0200

    done

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,31 @@
 import java.util.*;
 public class Main {
+
+     static int minimumNumber = 0;
+
     public static void main(String[] args) {
         String[] input = takeUserInput();
+        String[] filtered = filterWordsByLength(minimumNumber,input);
+        System.out.println(Arrays.toString(filtered));
+
     }
 
     private static String[] takeUserInput() {
-        while(true){
-            System.out.println("Please enter random words separated by space: ");
-
             Scanner sc = new Scanner(System.in);
-            String input = sc.next();
+            System.out.print("Please enter minimum chars number: ");
+            minimumNumber = sc.nextInt();
 
-        }
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter random words separated by space: ");
+            String input = scanner.nextLine();
+            String[] words = input.split("\\s+");
+            return words;
+
     }
+    public static String[] filterWordsByLength(int minLength, String[] words) {
+
+            return Arrays.stream(words)
+                    .filter(word -> word.length() >= minLength)
+                    .toArray(String[]::new);
+        }
 }
